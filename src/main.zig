85afcd171ff1ad3c0e8a31138d810774ecbad7ed
Jonata Novais Cirqueira <jonatanc0511@gmail.com>
2024-11-08 21:28:35 -0300

    feat: show failed letters

--- src/main.zig
+++ src/main.zig
@@ -6,8 +6,9 @@ const screenHeight = 1120;
 var sound_error: rl.Sound = undefined;
 
 pub fn main() anyerror!void {
-    rl.initWindow(screenWidth, screenHeight, "Roda a Roda");
+    rl.initWindow(screenWidth, screenHeight, "Show do Cristão");
     defer rl.closeWindow();
+    rl.toggleFullscreen();
 
     rl.setTargetFPS(60);
 
@@ -19,22 +20,15 @@ pub fn main() anyerror!void {
     rl.initAudioDevice();
     defer rl.closeAudioDevice();
     sound_error = rl.loadSound("resources/error.wav");
+    rl.setSoundVolume(sound_error, 5);
 
-    const file_content = @embedFile("biblical_terms.csv");
-    const line_count = std.mem.count(u8, file_content, "\n");
-    var rand = std.rand.DefaultPrng.init(@intCast(std.time.milliTimestamp()));
-    const lottery = @rem(rand.next(), line_count);
-    var lines = std.mem.splitAny(u8, file_content, "\r\n");
-    for (0..lottery) |_|
-        _ = lines.next();
-    var line = std.mem.splitAny(u8, lines.next().?, ",");
-    const term = line.next().?;
-    const tip = line.next().?;
+    const term = getRandomTerm();
     var msg: [80:0]u8 = undefined;
-    rl.traceLog(rl.TraceLogLevel.log_info, try std.fmt.bufPrintZ(&msg, "Dica: {s}", .{tip}));
+    rl.traceLog(rl.TraceLogLevel.log_info, try std.fmt.bufPrintZ(&msg, "Dica: {s}", .{term.tip}));
 
     var panel = try Panel.init();
-    try panel.setSecretWord(term);
+
+    try panel.setSecretWord(term.word);
 
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
         // Update
@@ -43,6 +37,13 @@ pub fn main() anyerror!void {
         if (letter_pressed != last_letter) {
             if (letter_pressed) |l| panel.testLetter(l);
             last_letter = letter_pressed;
+        }
+
+        if (rl.isKeyPressed(.key_enter)) {
+            const myterm = getRandomTerm();
+            var mymsg: [80:0]u8 = undefined;
+            rl.traceLog(rl.TraceLogLevel.log_info, try std.fmt.bufPrintZ(&mymsg, "Dica: {s}", .{myterm.tip}));
+            try panel.setSecretWord(myterm.word);
         }
 
         if (rl.isMouseButtonPressed(.mouse_button_left)) {
@@ -71,6 +72,34 @@ const Panel = struct {
     cards_line2: CardLine,
     cards_line3: CardLine,
     cards_line4: CardLine,
+    last_failed_letter: usize = 0,
+    failed_letters: [26]u8 = [_]u8{0} ** 26,
+
+    const failed_letters_postions: []const [2]f32 = &[_][2]f32{
+        [2]f32{ 0.441, 0.086 },
+        [2]f32{ 0.480, 0.086 },
+        [2]f32{ 0.518, 0.086 },
+        [2]f32{ 0.553, 0.086 },
+        [2]f32{ 0.401, 0.143 },
+        [2]f32{ 0.421, 0.143 },
+        [2]f32{ 0.446, 0.143 },
+        [2]f32{ 0.470, 0.143 },
+        [2]f32{ 0.499, 0.143 },
+        [2]f32{ 0.524, 0.143 },
+        [2]f32{ 0.554, 0.143 },
+        [2]f32{ 0.574, 0.143 },
+        [2]f32{ 0.595, 0.143 },
+        [2]f32{ 0.414, 0.188 },
+        [2]f32{ 0.440, 0.188 },
+        [2]f32{ 0.468, 0.188 },
+        [2]f32{ 0.500, 0.188 },
+        [2]f32{ 0.525, 0.188 },
+        [2]f32{ 0.554, 0.188 },
+        [2]f32{ 0.582, 0.188 },
+        [2]f32{ 0.463, 0.235 },
+        [2]f32{ 0.497, 0.235 },
+        [2]f32{ 0.528, 0.235 },
+    };
 
     pub fn init() !Panel {
         return .{
@@ -86,29 +115,59 @@ const Panel = struct {
         self.cards_line2.draw();
         self.cards_line3.draw();
         self.cards_line4.draw();
+
+        for (0..self.last_failed_letter) |i| {
+            if (self.failed_letters[i] == 0) @panic("failed_letters[i] == 0");
+            rl.drawText(
+                &[_:0]u8{self.failed_letters[i]},
+                @intFromFloat(failed_letters_postions[i][0] * screenWidth),
+                @intFromFloat(failed_letters_postions[i][1] * screenHeight),
+                30,
+                rl.Color.white,
+            );
+        }
     }
 
     pub fn setSecretWord(self: *Panel, word: []const u8) !void {
+        for (0..self.cards_line2.len) |i| {
+            self.cards_line2.cards[i].revealed = false;
+        }
+
         var buf: [15]u8 = undefined;
         const upper_case_word = std.ascii.upperString(&buf, word);
         if (upper_case_word.len > self.cards_line2.len) return error.PalavraMuitoGrande;
         for (0..upper_case_word.len) |i| {
-            if (upper_case_word[i] == ' ') continue;
+            if (upper_case_word[i] == ' ') {
+                self.cards_line2.cards[i].color = Card.color_off;
+                self.cards_line2.cards[i].secret_letter = ' ';
+            }
             self.cards_line2.cards[i].color = Card.color_on;
             self.cards_line2.cards[i].secret_letter = upper_case_word[i];
+        }
+        for (upper_case_word.len..self.cards_line2.len) |i| {
+            self.cards_line2.cards[i].color = Card.color_off;
+            self.cards_line2.cards[i].secret_letter = null;
         }
     }
 
     pub fn testLetter(self: *Panel, letter: rl.KeyboardKey) void {
+        const letter_u8 = @as(u8, @intCast(@intFromEnum(letter)));
         var found = false;
         for (&self.cards_line2.cards) |*card| {
-            if (card.secret_letter != null and card.secret_letter.? == @intFromEnum(letter)) {
+            if (card.secret_letter != null and card.secret_letter.? == letter_u8) {
                 card.revealed = true;
                 found = true;
             }
         }
-        if (!found) {
-            rl.playSound(sound_error);
+        block: {
+            if (!found) {
+                rl.playSound(sound_error);
+                for (self.failed_letters) |l| {
+                    if (l == letter_u8) break :block;
+                }
+                self.failed_letters[self.last_failed_letter] = letter_u8;
+                self.last_failed_letter += 1;
+            }
         }
     }
 
@@ -191,4 +250,20 @@ fn getLetterPressed() ?rl.KeyboardKey {
     if (rl.isKeyPressed(.key_y)) return .key_y;
     if (rl.isKeyPressed(.key_z)) return .key_z;
     return null;
+}
+
+// TODO passar pra dentro do Panel
+// TODO resetar failed_letters
+fn getRandomTerm() struct { tip: []const u8, word: []const u8 } {
+    const file_content = @embedFile("biblical_terms.csv");
+    const line_count = std.mem.count(u8, file_content, "\n");
+    var rand = std.rand.DefaultPrng.init(@intCast(std.time.milliTimestamp()));
+    const lottery = @rem(rand.next(), line_count);
+    var lines = std.mem.splitAny(u8, file_content, "\r\n");
+    for (0..lottery) |_|
+        _ = lines.next();
+    var line = std.mem.splitAny(u8, lines.next().?, ",");
+    const term = line.next().?;
+    const tip = line.next().?;
+    return .{ .tip = tip, .word = term };
 }
